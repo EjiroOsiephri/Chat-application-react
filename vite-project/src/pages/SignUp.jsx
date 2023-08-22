5ad8c40df89c686ac94538ee6f7ca0e63c068f7d
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-22 21:14:34 +0100

    Auth with email done suceesfully

--- vite-project/src/pages/SignUp.jsx
+++ vite-project/src/pages/SignUp.jsx
@@ -1,18 +1,58 @@
-import React, { useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import Logo from "../assets/chat.png";
 import google from "../assets/google.png";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Classes from "../sass/signup.module.scss";
+import AuthContext from "../context/Auth-context";
 
 const SignUp = () => {
   const [showLogin, setShowLogin] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  console.log(authCtx);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const loginStateHandler = () => {
     setShowLogin(true);
   };
   const signUpStateHandler = () => {
     setShowLogin(fasle);
+  };
+
+  const handleSubmitHandler = async (e) => {
+    e.preventDefault();
+    const emailValue = emailRef.current?.value;
+    const passwordValue = passwordRef.current?.value;
+    let url;
+    if (showLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCXaeR7W_OaiIEAN5o_BPmn8nT0R_-bShc";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCXaeR7W_OaiIEAN5o_BPmn8nT0R_-bShc";
+    }
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: emailValue,
+          password: passwordValue,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      authCtx.loginWithEmail(data.idToken);
+      console.log(data);
+    } catch (error) {
+      alert(error.message);
+    }
   };
   return (
     <>
@@ -36,14 +76,22 @@ const SignUp = () => {
           </div>
         )}
         <section className={Classes["form"]}>
-          <form>
+          <form onSubmit={handleSubmitHandler}>
             <div className={Classes["input-container"]}>
               <FaEnvelope className={Classes["input-icon"]} />
-              <input type="email" placeholder="Enter your email" />
+              <input
+                ref={emailRef}
+                type="email"
+                placeholder="Enter your email"
+              />
             </div>
             <div className={Classes["input-container"]}>
               <FaLock className={Classes["input-icon"]} />
-              <input type="password" placeholder="Enter your password" />
+              <input
+                ref={passwordRef}
+                type="password"
+                placeholder="Enter your password"
+              />
             </div>
             <div className={Classes["signinwithgoogle"]}>
               <img src={google} alt="" />

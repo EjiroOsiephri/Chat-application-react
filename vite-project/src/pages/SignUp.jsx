8b77cab6a6d9a453873b3f6e9da28bbc0e1a16ef
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-22 20:11:59 +0100

    Fixed login issue

--- vite-project/src/pages/SignUp.jsx
+++ vite-project/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/chat.png";
 import google from "../assets/google.png";
 import { FaEnvelope, FaLock } from "react-icons/fa";
@@ -6,6 +6,14 @@ import { Link } from "react-router-dom";
 import Classes from "../sass/signup.module.scss";
 
 const SignUp = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const loginStateHandler = () => {
+    setShowLogin(true);
+  };
+  const signUpStateHandler = () => {
+    setShowLogin(fasle);
+  };
   return (
     <>
       <main className={Classes["Signup-main"]}>
@@ -13,15 +21,20 @@ const SignUp = () => {
           <img src={Logo} alt="" />
           <h2>EjiroChatHub</h2>
         </div>
-        <div className={Classes["signup-text-section"]}>
-          <h1>Join thousands of users worldwide</h1>
-          <h2>
-            Simple, reliable, private messaging and calling for free*, available
-            all over the world. Never miss a moment with voice and video calls.
-            From a group call, to classmates ,to a quick call with mom, feel
-            like you’re in the same room with voice and video calls.
-          </h2>
-        </div>
+        {showLogin ? (
+          <h2 className={Classes["login-text"]}>Login</h2>
+        ) : (
+          <div className={Classes["signup-text-section"]}>
+            <h1>Join thousands of users worldwide</h1>
+            <h2>
+              Simple, reliable, private messaging and calling for free*,
+              available all over the world. Never miss a moment with voice and
+              video calls. From a group call, to classmates ,to a quick call
+              with mom, feel like you’re in the same room with voice and video
+              calls.
+            </h2>
+          </div>
+        )}
         <section className={Classes["form"]}>
           <form>
             <div className={Classes["input-container"]}>
@@ -39,8 +52,17 @@ const SignUp = () => {
             <button className={Classes["signup-btn"]}>Join us now</button>
           </form>
           <div className={Classes["login-section"]}>
-            <p>already have an account</p>
-            <Link>login</Link>
+            {showLogin ? (
+              <div>
+                <p>Don't have an account</p>
+                <Link onClick={signUpStateHandler}>Sign up</Link>
+              </div>
+            ) : (
+              <div>
+                <p>already have an account</p>
+                <Link onClick={loginStateHandler}>login</Link>
+              </div>
+            )}
           </div>
         </section>
       </main>

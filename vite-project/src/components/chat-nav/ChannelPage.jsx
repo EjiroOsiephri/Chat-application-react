df18e91e8c8ee3cf30079d8263213412f754f4df
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-23 16:09:44 +0100

    Starting off the chsnnel page

--- vite-project/src/components/chat-nav/ChannelPage.jsx
+++ vite-project/src/components/chat-nav/ChannelPage.jsx
@@ -5,6 +5,18 @@ import AuthContext from "../../context/Auth-context";
 const ChannelPage = () => {
   const ctx = useContext(AuthContext);
   console.log(ctx);
+
+  function getFirstLetters(name) {
+    const words = name.split(" ");
+    const firstLetters = words.map((word) => word.charAt(0));
+    return firstLetters.join("");
+  }
+
+  const firstLetters = ctx.users.map((user) => {
+    return getFirstLetters(user.displayName);
+  });
+
+  console.log(firstLetters);
   return (
     <>
       <main>
@@ -16,6 +28,19 @@ const ChannelPage = () => {
           <FaSearch className="search-logo" />
           <input type="text" placeholder="Search" />
         </div>
+        <section className="channelDiv">
+          <div className="welcome">
+            <h2>W</h2>
+            <h1>Welcome channel</h1>
+          </div>
+          {ctx.users.map((item, index) => {
+            return (
+              <div key={index}>
+                <h1>{item.displayName}</h1>
+              </div>
+            );
+          })}
+        </section>
       </main>
     </>
   );

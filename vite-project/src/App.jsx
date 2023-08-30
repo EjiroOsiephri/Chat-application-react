8118fe0e51bb0fe4713b8f17350828bff9bae441
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-30 10:24:56 +0100

    Introducing video sdk

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,17 +3,51 @@ import SignUp from "./pages/SignUp";
 import React, { Suspense, useContext, useState } from "react";
 import AuthContext from "./context/Auth-context";
 import AppWideContext from "./context/AppWideContext";
-
 import "./app.scss";
 import LoadingSpinner from "./components/LoadingSpinner";
 import Module from "./components/Modal";
 const ChannelHome = React.lazy(() =>
   import("./components/chat-application/ChannelHome")
 );
+import {
+  MeetingProvider,
+  MeetingConsumer,
+  useMeeting,
+  useParticipant,
+} from "@videosdk.live/react-sdk";
+import { authToken, createMeeting } from "../src/api/Api";
+import ReactPlayer from "react-player";
+
+function JoinScreen({ getMeetingAndToken }) {
+  return null;
+}
+
+function ParticipantView(props) {
+  return null;
+}
+
+function Controls(props) {
+  return null;
+}
+
+function MeetingView(props) {
+  return null;
+}
 
 function App() {
-  const ctx = useContext(AuthContext);
+  const [meetingId, setMeetingId] = useState(null);
 
+  const getMeetingAndToken = async (id) => {
+    const meetingId =
+      id == null ? await createMeeting({ token: authToken }) : id;
+    setMeetingId(meetingId);
+  };
+
+  const onMeetingLeave = () => {
+    setMeetingId(null);
+  };
+
+  const ctx = useContext(AuthContext);
   const [userChannel, setUserChannel] = useState(null);
   const [showNav, setShowNav] = useState(false);
   const [imgSrc, setImgSrc] = useState("");
@@ -40,6 +74,21 @@ function App() {
   };
   return (
     <>
+      {authToken && meetingId ? (
+        <MeetingProvider
+          config={{
+            meetingId,
+            micEnabled: true,
+            webcamEnabled: true,
+            name: "C.V. Raman",
+          }}
+          token={authToken}
+        >
+          <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
+        </MeetingProvider>
+      ) : (
+        <JoinScreen getMeetingAndToken={getMeetingAndToken} />
+      )}
       <AppWideContext.Provider value={contextValue}>
         {channel && <Module />}
         <Suspense
